07b5f2f0d0b72fb955b1ca88eebcbadfe564e5f1
carlos alberto baptista soares <carlosabsoares@gmail.com>
2020-03-08 13:50:02 -0300

    Estudos

---
+++ ProAgil.WebAPI/Data/DataContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using ProAgil.API.Model;
+
+namespace ProAgil.API.Data
+{
+    public class DataContext: DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options): base(options){}
+
+        public DbSet<Evento> Eventos { get; set; }
+    }
+}
